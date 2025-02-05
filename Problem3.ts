eec29cb41cc35b74f8e88143c2bc3bba1478547c
MRiDuL-ICE <abdulwahab22400@gmail.com>
2025-02-05 14:56:49 +0600

    solved the problem using different approach

--- Problem3.ts
+++ Problem3.ts
@@ -1,9 +1,9 @@
 {
-    const countWordOccurrences = (sen: string, word: string): number => {
-        const words = (sen.match(word) || []).length
-        return words
+    const countWordOccurrences = (sen: string, emni: string): number => {
+        const words = sen.split(" ")
+        return words.filter(word => word === emni).length
     }
 
-    let output = countWordOccurrences("I love typescript", "love typescript ");
+    let output = countWordOccurrences("I love typescript", "typescript");
     console.log(output)
 }
